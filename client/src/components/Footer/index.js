7889f8ece3e080031185701bf085f5c67a3f09da
Chris Zichko <chriszichkocoding@gmail.com>
2022-03-08 09:26:41 -0500

    Footer component is in place, but still needs editting with the correct css

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Footer() {
+    
+    // automates the year being added in Footer for copyright
+    function fullYear() {
+        const yearFormat = new Date();
+        return yearFormat.getFullYear();
+    };
+
+    return (
+        <>
+            <div class="row">
+                <div class="main-footer2">
+                    <h6 class="banner-text">
+                        Technical Union | Industrial Exports | Pumps | Motors | Valves |
+                        Filters | Flowmeters | Servo Valves | Servo Motors | Manifolds |
+                        Accumulators | Oil Analysis | General Industrial Supplies..
+                    </h6>
+                </div>
+            </div>
+            <div class="row">
+                <footer class="main-footer">
+                <p class="copyright">
+                    © 2022 Copyright Technical Union <br />&nbsp;&nbsp; | Developed by
+                    Edgar Lindo & <b>ZichKoding</b> |
+                </p>
+                </footer>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
