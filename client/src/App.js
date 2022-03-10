3295bde87bdb935fc4a0d878b24b871c21466677
Chris Zichko <chriszichkocoding@gmail.com>
2022-03-10 13:25:13 -0500

    All of the Product pages have been converted to React. Table styling still needs fixed for every product page

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,13 @@ import Footer from './components/Footer';
 import Homepage from './pages/Homepage';
 import Products from './pages/Products';
 import VanePumps from './pages/VanePumps';
+import GearPumps from './pages/GearPumps';
+import GerolerMotors from './pages/GerolerMotors';
+import VaneMotors from './pages/VaneMotors';
+import FlowMeters from './pages/FlowMeters';
+import DirectionalControlValves from './pages/DirectionalControlValves';
+import FlowControlValves from './pages/FlowControlValves';
+import Manifolds from './pages/Manifolds';
 
 function App() {
   return (
@@ -23,8 +30,16 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Homepage />} />
+
           <Route path="/Products" element={<Products />} />
-          <Route path="/Products/VanePumps" element={<VanePumps />} />
+            <Route path="/Products/VanePumps" element={<VanePumps />} />
+            <Route path="/Products/GearPumps" element={<GearPumps />} />
+            <Route path="Products/GerolerMotors" element={<GerolerMotors />} />
+            <Route path="/Products/VaneMotors" element={<VaneMotors />} />
+            <Route path="/Products/FlowMeters" element={<FlowMeters />} />
+            <Route path="/Products/DirectionalControlValves" element={<DirectionalControlValves />} />
+            <Route path="/Products/FlowControlValves" element={<FlowControlValves />}  />
+            <Route path="/Products/Manifolds" element={<Manifolds />} />
           
         </Routes>
 
---
+++ client/src/pages/VaneMotors.js
@@ -0,0 +1,77 @@
+import React from "react";
+import ProductForm from "../components/ProductForm";
+import ProductNav from "../components/ProductNav";
+
+
+function VaneMotors() {
+    return (
+        <>
+            <div class="row">
+                <div class=" col-md-9">
+                    <h6 class="product-title-text"> Vane Motors</h6> <br /> 
+                    <img src ={require("../assets/images/vane-motors.png")} class= "product-pic" alt="vane-motors" /> <br />
+                    <a href={require("../assets/pdf files/hydraulic-vane-motor-catalog.pdf")} class="btn btn-info">Download PDF</a> <br /> <br />
+
+                    <table class= "table table-hover table-striped table-bordered">
+                        <tr>
+                            <thead />
+                            <th colspan="4" class="product-title-table-text">Vane Motors </th>
+                        </tr>
+                        
+                        <tr class="inside-table-text">
+                            <td>Series</td>
+                            <td>Displacement <br /> (cu/in²)</td>
+                            <td>Max Torque <br /> (lb. in.) <br /> at max psi</td>
+                            <td> Max rpm and psi</td>
+                        </tr>
+      
+     
+                        <tr class="inside-table-text">
+                            <td>25M</td>
+                            <td>2.68 <br /> 3.52 <br /> 4.19</td>
+                            <td>1,025 <br /> 1,340 <br /> 1,600</td>
+                            <td rowspan="3"> 3,600 rpm at 500 psi * <br />  4,000 rpm at 500 psi ** 
+                            <br /> 2,600 rpm at 2,250 psi* <br /> 3,000 rpm at 2,500 psi **</td>
+                        </tr>
+
+                        <tr class="inside-table-text">
+                            <td>35M</td>
+                            <td>5.10 <br /> 6.12 <br /> 7.44</td>
+                            <td>1,960 <br /> 2,340 <br /> 2,840</td>
+                        </tr>
+
+                        <tr class="inside-table-text">
+                            <td>45M</td>
+                            <td>8.42 <br /> 9.96 <br /> 11.79</td>
+                            <td>3,200 <br /> 3,800 <br /> 4,450</td>
+                        </tr>
+
+                        <tr class="inside-table-text">
+                            <td>50M</td>
+                            <td>14.11<br /> 16.36 <br /> 19.35</td>
+                            <td>5,450 <br /> 6,350 <br /> 7,470</td>
+                            <td> 2,800 rpm at 500 psi * <br />  3,200 rpm at 500 psi ** 
+                            <br /> 2,200 rpm at 2,250 psi* <br /> 2,400 rpm at 2,500 psi **</td>
+                        </tr>
+
+                        <tr>
+                            <td colspan="4">
+                            &nbsp; * Continuous operation. <br />
+                            &nbsp; ** Intermittent operation. <br />
+                            &nbsp; *** 114 model suffix: 2,500 psi counter clockwise / 2,250 psi clockwise. <br />
+                            &nbsp; *** 124 model suffix: 2,500 psi bi-rotational rotation. <br />
+                            &nbsp; - Rotation view from shaft end.
+                            </td>
+
+                        </tr>
+                    </table>
+                    <br />
+                    <ProductForm />
+                </div>
+                <ProductNav />
+            </div>        
+        </>
+    );
+};
+
+export default VaneMotors;
