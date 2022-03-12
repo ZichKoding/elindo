51b91c3b11cdb3a8623436fe706fef8849f30347
Chris Zichko <chriszichkocoding@gmail.com>
2022-03-12 12:50:54 -0500

    trying .env out for deploying to heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 const db = require('./config/connection');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
