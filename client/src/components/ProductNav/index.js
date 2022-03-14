ddb93cf9e94729e2ac2c22afa6e46f7458424fc1
Chris Zichko <chriszichkocoding@gmail.com>
2022-03-14 10:13:03 -0400

    Product nav active link is now working properly

--- client/src/components/ProductNav/index.js
+++ client/src/components/ProductNav/index.js
@@ -1,63 +1,74 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function ProductNav() {
+    const [pathName, setPathName] = useState();
+
+    useEffect(() => {
+        const url = window.location.pathname;
+        setPathName(url);
+        console.log(pathName);
+    });
+
+
+
     return (
-        <div class=" col-md-3">
-            <h3 class="nav2title">PRODUCTS</h3>
-            <ul class="nav2">
+        <div className=" col-md-3">
+            <h3 className="nav2title">PRODUCTS</h3>
+            <ul className="nav2">
                 <li>
-                    <h5 class="link1" href="#">Pumps</h5>
+                    <h5 className="link1" href="#">Pumps</h5>
                     <dl>
                         <dt>
                             {/* Might need to useState on which link is active */}
-                            <Link to="/Products" class="link2 active2"> Piston Pumps  </Link> 
+                            <Link to="/Products" className={pathName === "/Products" ? "link2 active2" : "link2"} > Piston Pumps  </Link> 
                         </dt>
                         <dt>
-                            <Link to="/Products/VanePumps" class="link2"  href="/Products/vanepumps.html"> Vane Pumps </Link> 
+                            <Link to="/Products/VanePumps" className={pathName === "/Products/VanePumps" ? "link2 active2" : "link2"}  href="/Products/vanepumps.html" > Vane Pumps </Link> 
                         </dt>
                         <dt>
-                            <Link to="/Products/GearPumps" class="link2"> Gear Pumps </Link> 
+                            <Link to="/Products/GearPumps" className={pathName === "/Products/GearPumps" ? "link2 active2" : "link2"} > Gear Pumps </Link> 
                         </dt>
                     </dl>
                 </li>
     
                 <li >
-                    <h5 class="link1" href="#">Motors</h5>
+                    <h5 className="link1" href="#">Motors</h5>
                     <dl>
                         <dt>
-                            <Link to="/Products/GerolerMotors" class="link2"> Geroler Motors  </Link> 
+                            <Link to="/Products/GerolerMotors" className={pathName === "/Products/GerolerMotors" ? "link2 active2" : "link2"}> Geroler Motors  </Link> 
                         </dt>
                         <dt>
-                            <Link to="/Products/VaneMotors" class="link2"> Vane Motors </Link> 
+                            <Link to="/Products/VaneMotors" className={pathName === "/Products/VaneMotors" ? "link2 active2" : "link2"}> Vane Motors </Link> 
                         </dt>
                         
                     </dl>
                 </li>
                 <li >
                     <dl>
-                        <h5 class="link1"  href="#">Flowmeters</h5>
+                        <h5 className="link1"  href="#">Flowmeters</h5>
                         <dt>
-                            <Link to="/Products/FlowMeters" class="link2"> Flowmeters  </Link> 
+                            <Link to="/Products/FlowMeters" className={pathName === "/Products/FlowMeters" ? "link2 active2" : "link2"}> Flowmeters  </Link> 
                         </dt>
                     </dl>
                 </li>
                 <li >
-                    <h5 class="link1" href="#">Valves</h5>
+                    <h5 className="link1" href="#">Valves</h5>
                     <dl>
                         <dt>
-                            <Link to="/Products/DirectionalControlValves" class="link2"> Directional Control  </Link> 
+                            <Link to="/Products/DirectionalControlValves" className={pathName === "/Products/DirectionalControlValves" ? "link2 active2" : "link2"}> Directional Control  </Link> 
                         </dt>
                         <dt>
-                            <Link to="/Products/FlowControlValves" class="link2"> Flow Control </Link> 
+                            <Link to="/Products/FlowControlValves" className={pathName === "/Products/FlowControlValves" ? "link2 active2" : "link2"} > Flow Control </Link> 
                         </dt>
                     </dl>
                 </li>
                 <li>
-                    <h5 class="link1"> Manifolds</h5>
+                    <h5 className="link1"> Manifolds</h5>
                     <dl>
                         <dt>
-                            <Link to="/Products/Manifolds" class="link2"> Manifolds</Link>
+                            <Link to="/Products/Manifolds" className={pathName === "/Products/Manifolds" ? "link2 active2" : "link2"}> Manifolds</Link>
                         </dt>
                     </dl>
                 </li>
